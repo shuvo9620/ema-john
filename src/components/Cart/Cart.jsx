132f5cba99df3bfcdd76fd8144983469548ac6aa
shuvo9620 <smahfuzur0@gmail.com>
2023-04-08 16:35:54 +0600

    clear cart button added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
     //const cart = props.cart; // option-1
     //const {cart} = props; // option-2
 
     let totalPrice = 0;
     let totalShipping = 0;
     let quantity = 0;
-    for (const product of cart){
+    for (const product of cart) {
         product.quantity = product.quantity || 1;
         totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
         quantity = quantity + product.quantity;
     }
 
-    const tax = totalPrice*7/100;
+    const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
 
     return (
@@ -26,6 +28,10 @@ const Cart = ({cart}) => {
             <p>Total Shipping: ${totalShipping}  </p>
             <p>Tax: ${tax.toFixed(2)} </p>
             <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+            <button className='btn-clr'>
+                <span>Clear Cart</span>
+                <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
