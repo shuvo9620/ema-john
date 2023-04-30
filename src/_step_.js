54ff523dafe249f5af246eabe4c9818cf29f4cbd
shuvo9620 <smahfuzur0@gmail.com>
2023-05-01 02:29:39 +0600

    show & hide password

---
+++ src/_step_.js
@@ -0,0 +1,38 @@
+/**
+ * 1. create a firebase project
+ * 2. create web app
+ * 3. npm install firebase
+ * 4. save firebase config & export default app
+ * 5. Build > Authentication > get started > enable sign in method
+ * 6. create sign up and Login route 
+*/
+
+/**
+ * 1. create a context provider file
+ * 2. create a context & set provider
+ * 3. set the children props
+ * 4. set context value
+ * 5. set the provider 
+*/
+
+/**
+ * HOSTING
+ * --------------------
+ * one time per pc
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 
+ * FOR EACH PROJECT ONE TIME
+ * 1. firebase init
+ * 2.proceed
+ * 3. hosting: firebase (up & down arrow) use space bar to select
+ * 4. existing project
+ * 5. select project carefully
+ * 6. which project as public directory: dist
+ * 7. single page application: yes
+ * 8. continuous deployment: n
+ * 
+ * FOR EVERY TIME DEPLOY
+ * 1. npm run build
+ * 2. firebase deploy
+*/
