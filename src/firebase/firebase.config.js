ad4df15ceda7948f796fba34ac4e36fcf54da544
shuvo9620 <smahfuzur0@gmail.com>
2023-05-01 01:52:05 +0600

    firebase authentica & private route + redirect to the current page completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgz1Uio3c5qj7zSU7Alow8DnErfj6aPmg",
+  authDomain: "ema-john-with-firebase-a-b5376.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-b5376",
+  storageBucket: "ema-john-with-firebase-a-b5376.appspot.com",
+  messagingSenderId: "1044735276693",
+  appId: "1:1044735276693:web:934db902ebe1968dd14d9c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
